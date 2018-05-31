7464aaf69505edccf2fe3ed4a87fb8416d338c1b
sharmindran <sharmindranraj@gmail.com>
2018-05-31 17:41:03 +0800

    Ready for heroku

--- server.js
+++ server.js
@@ -100,6 +100,6 @@ server.get('/form', (req, res) => {
   res.render('form.hbs');
 });
 
-server.listen(3000, () => {
-  console.log("Server listening on port 3000");
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
