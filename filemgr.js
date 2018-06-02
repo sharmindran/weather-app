a83928571010c2bb31d365923d5c478cc81567b7
sharmindran <sharmindranraj@gmail.com>
2018-06-02 12:44:31 +0800

    Added mlab mongodb

--- filemgr.js
+++ filemgr.js
@@ -1,7 +1,8 @@
 const {MongoClient} = require('mongodb');
 const fs = MongoClient;
 
-const database = 'mongodb://localhost:27017';
+//const database = 'mongodb://localhost:27017';
+const database = 'mongodb://<lab123>:<lab123>@ds245240.mlab.com:45240/weather-app'
 
 const saveData = (newdata) => {
   return new Promise((resolve, reject) => {
